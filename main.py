e8b6bcb2d35ccc87e6d8443d9478a1e8dc70c34a
Hayas92 <medellelsayah@gmail.com>
2022-07-27 11:41:13 +0200

    add class

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import matplotlib.pyplot as plt
 import scipy.stats as stats
 import csv
+import numpy
+import random
+
+from math import *
 
 FILENAME = "data/dataset_1_brca.csv"
 DELIMITER = ","
+FILTERED_HEADERS = ['']
+MISSING_VALUES = [-1]
 
 
 def getColumn(data, index):
@@ -29,6 +36,48 @@ def convertToNumeric(data):
     return True
 
 
+def convertToHistogram(data):
+    histo = {}
+    for d in data:
+        if d in histo:
+            histo[d] += 1
+        else:
+            histo[d] = 1
+    return histo
+
+
+class UnivariateStats:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        self.isNumeric = convertToNumeric(self.data)
+
+        if self.name in FILTERED_HEADERS:
+            self.data = list(filter(lambda x: x not in MISSING_VALUES, self.data))
+
+        self.histo = convertToHistogram(self.data)
+
+        if self.isNumeric:
+            self.skew = stats.skew(self.data)
+            self.kurtosis = stats.kurtosis(self.data)
+
+    def __repr__(self):
+        res = f'{self.name}'
+
+        nbValues = len(self.histo)
+        res += f'\n   {nbValues} values'
+        if nbValues == len(self.data):
+            res += f'\n   INDEX'
+        elif nbValues > 1:
+            res += f'\n   {min(self.data)} ... {max(self.data)}'
+        else:
+            res += f'\n   {self.data[0]}'
+
+        if self.isNumeric:
+            res += f'\n   S:{self.skew:.1f}\tK:{self.kurtosis:.1f}'
+        return res
+
+
 if __name__ == "__main__":
     data = []
     with open(FILENAME, newline='') as csvfile:
@@ -42,3 +91,9 @@ if __name__ == "__main__":
 
     # Data shape
     print(f'{len(headers)} variables x {len(data)} lines')
+
+    variables = []
+    for i in range(len(headers)):
+        uv = UnivariateStats(headers[i], getColumn(data, i))
+        variables.append(uv)
+        print(uv)
